6ffc5e98cbe4eb18075de6122af863893341bab9
Gero <gersonhoa@gmail.com>
2022-06-08 12:39:27 -0300

    feat : add object in typenotation

--- modulo-02/myMake/typeAnnotation.js
+++ modulo-02/myMake/typeAnnotation.js
@@ -2,3 +2,10 @@ var nome = 'Gero';
 console.log(nome);
 var animais = ['gato', 'cachorro', 'passarinho'];
 console.log(animais);
+var carro;
+carro = { nome: "toyota", ano: 2020, preço: 800 };
+console.log(carro);
+function mult(a, b) {
+    return a * b;
+}
+console.log(mult(5, 5));
--- modulo-02/myMake/typeAnnotation.ts
+++ modulo-02/myMake/typeAnnotation.ts
@@ -3,3 +3,18 @@ console.log(nome);
 
 let animais: string[] = ['gato','cachorro','passarinho'];
 console.log(animais);
+
+let carro :{
+  nome:string,
+  ano: number,
+  preço: number,
+};
+
+carro = {nome: "toyota", ano:2020, preço: 800 }
+console.log(carro)
+
+function mult (a:number, b:number){
+  return a * b
+}
+
+console.log(mult(5,5));
